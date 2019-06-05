36539014eaec39c2ca2698d19562c7f0cf161a28
phone-e-z <ax152147@gmail.com>
2019-06-05 22:23:06 +0800

    Removed folder from repository

--- slave_device/.slave_device.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/cnlab/project2/slave_device/slave_device.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/cnlab/project2/slave_device/slave_device.ko /home/cnlab/project2/slave_device/slave_device.o /home/cnlab/project2/slave_device/slave_device.mod.o ;  true
