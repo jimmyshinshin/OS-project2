cab0036d8edc88ca2107c1878c1201c0271843cd
phone-e-z <ax152147@gmail.com>
2019-06-10 21:02:28 +0800

    add some functions

--- slave_device/slave_device.c
+++ slave_device/slave_device.c
@@ -31,7 +31,7 @@
 
 
 #define BUF_SIZE 512
-
+#define MAP_SIZE PAGE_SIZE * 100
 
 
 
@@ -59,13 +59,47 @@ static mm_segment_t old_fs;
 static ksocket_t sockfd_cli;//socket to the master server
 static struct sockaddr_in addr_srv; //address of the master server
 
+static int mmap_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+	vmf->page = virt_to_page(vma->vm_private_data);
+	get_page(vmf->page);
+	return 0;
+}
+void mmap_open(struct vm_area_struct *vma)
+{
+	/* Do nothing */
+}
+void mmap_close(struct vm_area_struct *vma)
+{
+	/* Do nothing */
+}
+static const struct vm_operations_struct my_vm_ops = {
+	.open = mmap_open,
+	.close = mmap_close,
+	.fault = mmap_fault
+};
+
+static int my_mmap(struct file *file, struct vm_area_struct *vma)
+{
+	io_remap_pfn_range(vma,
+		vma->vm_start,
+		virt_to_phys(file->private_data) >> PAGE_SHIFT,
+                vma->vm_end - vma->vm_start,
+		vma->vm_page_prot);
+	vma->vm_ops = &my_vm_ops;
+	vma->vm_flags |= VM_RESERVED;
+	vma->vm_private_data = file->private_data;
+	mmap_open(vma);
+	return 0;
+}
 //file operations
 static struct file_operations slave_fops = {
 	.owner = THIS_MODULE,
 	.unlocked_ioctl = slave_ioctl,
 	.open = slave_open,
 	.read = receive_msg,
-	.release = slave_close
+	.release = slave_close,
+	.mmap = my_mmap
 };
 
 //device info
@@ -101,11 +135,13 @@ static void __exit slave_exit(void)
 
 int slave_close(struct inode *inode, struct file *filp)
 {
+	kfree(filp->private_data);
 	return 0;
 }
 
 int slave_open(struct inode *inode, struct file *filp)
 {
+	filp->private_data = kmalloc(MAP_SIZE, GFP_KERNEL);
 	return 0;
 }
 static long slave_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
@@ -113,14 +149,15 @@ static long slave_ioctl(struct file *file, unsigned int ioctl_num, unsigned long
 	long ret = -EINVAL;
 
 	int addr_len ;
-	unsigned int i;
-	size_t len, data_size = 0;
+	//unsigned int i;
+	//size_t len, data_size = 0;
+	size_t offset = 0, rec_n;
 	char *tmp, ip[20], buf[BUF_SIZE];
-	struct page *p_print;
-	unsigned char *px;
+	//struct page *p_print;
+	//unsigned char *px;
 
     pgd_t *pgd;
-	p4d_t *p4d;
+	//p4d_t *p4d;
 	pud_t *pud;
 	pmd_t *pmd;
     pte_t *ptep, pte;
@@ -163,7 +200,15 @@ static long slave_ioctl(struct file *file, unsigned int ioctl_num, unsigned long
 			ret = 0;
 			break;
 		case slave_IOCTL_MMAP:
-
+			while (1) {
+				rec_n = krecv(sockfd_cli, buf, sizeof(buf), 0);
+				if (rec_n == 0) {
+					break;
+				}
+				memcpy(file->private_data + offset, buf, rec_n);
+				offset += rec_n;
+			}
+			ret = offset;
 			break;
 
 		case slave_IOCTL_EXIT:
@@ -176,16 +221,16 @@ static long slave_ioctl(struct file *file, unsigned int ioctl_num, unsigned long
 			break;
 		default:
             pgd = pgd_offset(current->mm, ioctl_param);
-			p4d = p4d_offset(pgd, ioctl_param);
-			pud = pud_offset(p4d, ioctl_param);
-			pmd = pmd_offset(pud, ioctl_param);
+			//p4d = p4d_offset(pgd, ioctl_param);
+			//pud = pud_offset(pgd, ioctl_param);
+			pmd = pmd_offset(pgd, ioctl_param);
 			ptep = pte_offset_kernel(pmd , ioctl_param);
 			pte = *ptep;
 			printk("slave: %lX\n", pte);
 			ret = 0;
 			break;
 	}
-    set_fs(old_fs);
+	set_fs(old_fs);
 
 	return ret;
 }
